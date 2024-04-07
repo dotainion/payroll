f94f87baf0586f5cfb7dfc42c71fe95ae48a40e7
dotainion <58013018+dotainion@users.noreply.github.com>
2024-04-06 21:08:48 -0400

    refactoring

--- src/pages/EmployeeSettings.js
+++ src/pages/EmployeeSettings.js
@@ -1,12 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BankGenerator } from "../widgets/BankGenerator";
 import { api } from "../request/Api";
-import { useParams } from "react-router-dom";
-import { BsFillShieldLockFill } from "react-icons/bs";
-import $ from 'jquery';
-import { payload } from "../utils/AddonsPayload";
+import { useParams, useNavigate } from "react-router-dom";
 import { toast } from "../utils/Toast";
 import { EmployeeSettingCard } from "../components/EmployeeSettingCard";
+import { routes } from "../router/routes";
 
 export const EmployeeSettings = () =>{
     const [loading, setLoading] = useState(true);
@@ -14,6 +12,7 @@ export const EmployeeSettings = () =>{
     const [existingBanks, setExistingBanks] = useState([]);
 
     const params = useParams();
+    const navigate = useNavigate();
 
     const onAssignUserCredential = () =>{
         api.user.assignCredential(params?.userId).then((response)=>{
@@ -41,6 +40,18 @@ export const EmployeeSettings = () =>{
 
     return(
         <div className="container">
+            <div className="employee-header px-5">
+                <div>
+                    <button onClick={()=>navigate(routes.workspace().nested().createEmployee())} className="btn mx-2 small">
+                        <BsPersonFillAdd className="text-dark p-2 fs-1 rounded-circle bg-light"/>
+                        <div className="small text-light"><small><small>New employee</small></small></div>
+                    </button>
+                    <button onClick={()=>navigate(routes.workspace().nested().createReport(params?.userId))} className="btn mx-2 small">
+                        <BiSolidReport className="text-dark p-2 fs-1 rounded-circle bg-light"/>
+                        <div className="small text-light"><small><small>Create report</small></small></div>
+                    </button>
+                </div>
+            </div>
             <div className="bg-light p-3 rounded-3 my-2">
                 <div>Password</div>
                 {!loading && <EmployeeSettingCard hasCredential={hasCredential} onAssignClick={onAssignUserCredential} />}  
